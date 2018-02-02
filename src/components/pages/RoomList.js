32efd4556a1bc906c7407b6ae32658d18d4ca51f
Anson Chung <anson.chung@gmail.com>
2018-02-02 14:53:26 -0500

    placeholder markup for roomlist

--- src/components/pages/RoomList.js
+++ src/components/pages/RoomList.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 const RoomListPage = () =>
   <div>
     <h1>Room List</h1>
+    <RoomList/>
   </div>
 
+class RoomList extends Component {
+
+  get showRooms() {
+    return <li>Test <button onClick={this.handleJoin}>Join</button></li>
+  }
+
+  handleJoin() {
+    alert('Join Room');
+  }
+
+  render() {
+    return (
+      <ul>{this.showRooms}</ul>
+    );
+  }
+}
 
 export default RoomListPage;
