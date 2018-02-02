e528e17d7227fd714c0afdc980ea3aaa79709191
Anson Chung <anson.chung@gmail.com>
2018-02-02 08:34:12 -0500

    adding google sign in

---
+++ src/components/GoogleSignIn.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+import { auth } from '../firebase';
+import * as routes from '../constants/routes';
+
+const byPropKey = (propertyName, value) => () => ({
+  [propertyName]: value,
+});
+
+class GoogleSignIn extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  googleSignIn(event) {  
+    const {
+      history,
+    } = this.props;
+
+    auth.doSignInWithGoogle()
+      .then(() => {
+        history.push(routes.HOME);
+      })
+      .catch(error => {
+        this.setState(byPropKey('error', error));
+      });    
+  }
+
+  render() {
+    return (
+      <button onClick={() => this.googleSignIn()}>
+        Google Sign In
+      </button>
+    );
+  }
+}
+
+export default GoogleSignIn;
--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,13 +1,19 @@
-import { auth } from './firebase';
+import * as firebase from 'firebase';
 
 // Sign Up
 export const doCreateUserWithEmailAndPassword = (email, password) =>
-  auth.createUserWithEmailAndPassword(email, password);
+  firebase.auth().createUserWithEmailAndPassword(email, password);
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
+export function doSignInWithGoogle() {
+    return firebase.auth().signInWithRedirect(googleProvider);
+}
 
   // Sign In
 export const doSignInWithEmailAndPassword = (email, password) =>
-auth.signInWithEmailAndPassword(email, password);
+  firebase.auth().signInWithEmailAndPassword(email, password);
 
 // Sign out
 export const doSignOut = () =>
-  auth.signOut();
+  firebase.auth().signOut();
