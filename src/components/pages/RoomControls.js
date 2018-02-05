daccdf347d4126eb29fbf9026ea4ec3b86007c59
Anson Chung <anson.chung@gmail.com>
2018-02-05 09:30:43 -0500

    implementing previous button

--- src/components/pages/RoomControls.js
+++ src/components/pages/RoomControls.js
@@ -1,23 +1,63 @@
 import React, { Component } from 'react';
+import { firebase } from '../../firebase';
 
 const RoomControlsPage = ({match}) =>
   <div>
-    <h1>Room Controls {match.params.roomId}</h1>
+    <h1>Room Controls</h1>
 
     <div>
-      <PreviousButton/>
-      <NextButton/>
+      <PreviousButton room-id={match.params.roomId}/>
+      <NextButton room-id={match.params.roomId}/>
     </div>
 
     <div>
-      <QuizSetupForm/>
+      <QuizSetupForm room-id={match.params.roomId}/>
     </div>
   </div>
 
 
 class PreviousButton extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      numQuestions: 0
+    }
+  }
+
+  componentWillReceiveProps(newProps) {
+    var vm = this;
+
+    var roomId = newProps['room-id'];
+    vm.setState({roomId: roomId});
+
+    if (roomId) {
+      var docRef = firebase.firestore.collection('rooms').doc(roomId);
+
+      docRef.onSnapshot(function(roomSnapshot) {
+              console.log(roomSnapshot.data());
+              vm.setState({currentQuestion: roomSnapshot.data()['current-question']});
+            });
+
+      docRef.collection('questions')
+            .onSnapshot(function(questionsSnapshot) { 
+              vm.setState({numQuestions: questionsSnapshot.size});
+            });
+    }
+  }
+
   handleClick(event) {
-    alert('Previous was clicked');
+    var vm = this;
+
+    if (vm.state.roomId && vm.state.currentQuestion) {
+      console.log('vm.state.currentQuestion', vm.state.currentQuestion);
+      firebase.firestore
+              .collection('rooms')
+              .doc(vm.state.roomId)
+              .update({
+                 'current-question': vm.state.currentQuestion-1
+              });
+    }
   }
 
   render() {
