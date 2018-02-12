b86b87ae9de6ec379312cc62cea0c50fa4492b20
Kevin Chan <kvii.chan@gmail.com>
2018-02-12 16:06:56 -0500

    added question submit functionality

--- src/components/pages/Quiz.js
+++ src/components/pages/Quiz.js
@@ -3,7 +3,7 @@ import { firebase } from '../../firebase';
 
 const QuizPage = ({match}) =>
   <div>
-    <QuizComponent room-id={match.roomId}/>>
+    <QuizComponent room-id={match.params.roomId}/>>
   </div>
 
 class QuizComponent extends Component {
@@ -15,54 +15,77 @@ class QuizComponent extends Component {
       currentQuestion: 0,
       question: '',
       code: '',
-      answers: []
+      hasMultipleCorrect: false,
+      answers: [],
+      score: 0,
+      usersRef: null
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentWillReceiveProps(newProps) {
-    var vm = this;
+    let vm = this;
 
-    var roomId = newProps['room-id'];
+    let roomId = newProps['room-id'];
     vm.setState({roomId: roomId});
 
     if (roomId) {
       var docRef = firebase.firestore.collection('rooms').doc(roomId);
 
       docRef.onSnapshot(function(roomSnapshot) {
-              var questionNumber = roomSnapshot.data().currentQuestion;
-              vm.setState({currentQuestion: questionNumber});
-              vm.setState({showAnswer: roomSnapshot.data().showAnswer});
+        var questionNumber = roomSnapshot.data().currentQuestion;
+        vm.setState({currentQuestion: questionNumber});
+        vm.setState({showAnswer: roomSnapshot.data().showAnswer});
 
-              docRef.collection('questions')
-                    .where('number', '==', questionNumber)
-                    .onSnapshot(function(questionSnapshot) {
-                      var questionData = questionSnapshot.docs[0].data();
-                      vm.setState({
-                        question: questionData.question,                        
-                        code: questionData.code
-                      });
-
-                      docRef.collection('questions')
-                            .doc(questionSnapshot.docs[0].id)
-                            .collection('answers')
-                            .get()
-                            .then(function(answersSnapshot) {
-                              var answers = []
-                              answersSnapshot.forEach(function(answer) {
-                                answers.push({
-                                  id: answer.data().id, 
-                                  answer: answer.data().answer, 
-                                  correct: answer.data().correct,
-                                  points: answer.data().points
-                                });
-                              });
-                              vm.setState({
-                                'answers': answers,
-                                'fetched': true
-                              });
-                            })
-                  });
+        docRef.collection('questions')
+          .where('number', '==', questionNumber)
+          .onSnapshot(function(questionSnapshot) {
+            var questionData = questionSnapshot.docs[0].data();
+            vm.setState({
+              question: questionData.question,                        
+              code: questionData.code,
+              hasMultipleCorrect: questionData.hasMultipleCorrect
             });
+
+            document.getElementById('question-form').reset();
+
+            let usersRef = docRef.collection('users');
+            vm.setState({usersRef: usersRef});
+
+            docRef.collection('questions')
+              .doc(questionSnapshot.docs[0].id)
+              .collection('answers')
+              .get()
+              .then(function(answersSnapshot) {
+                var answers = []
+                answersSnapshot.forEach(function(answer) {
+                  answers.push({
+                    id: answer.data().id, 
+                    answer: answer.data().answer, 
+                    correct: answer.data().correct,
+                    points: answer.data().points
+                  });
+                });
+                vm.setState({
+                  'answers': answers,
+                  'fetched': true
+                });
+              });
+
+            usersRef.where('email', '==', firebase.user.email)
+              .get().then(users => {
+                if (users.size === 0) {
+                  usersRef.doc(firebase.user.uid).set({
+                    name: firebase.user.displayName,
+                    email: firebase.user.email,
+                    score: 0
+                  });
+                }                
+              });
+            });    
+        });
     }
   }
 
@@ -81,12 +104,27 @@ class QuizComponent extends Component {
   get renderAnswers() {
     const answers = this.state.answers;
     return answers.map((answer, index) => {
-      return <label key={index} id={answer.id} data-correct={answer.correct} className={this.isCorrect}>{answer.answer}<input type="checkbox" value={answer.points}/></label>
+      return <label key={index} id={answer.id} data-correct={answer.correct} className={this.isCorrect}>
+        {answer.answer}<input name={this.state.currentQuestion} type={this.state.hasMultipleCorrect ? "checkbox" : "radio"} value={answer.points} onChange={this.handleChange}/></label>
     });
   }
 
-  handleSubmit() {
+  handleChange(event) {
+    let target = event.target;
 
+
+    if (target.value) {
+      let points = parseInt(target.checked ? target.value : -target.value);
+      let newScore = this.state.score + points;
+      this.setState({
+        score: newScore
+      })
+    }
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.state.usersRef.doc(firebase.user.uid).update({score: this.state.score});
   }
 
   render() {
@@ -98,7 +136,7 @@ class QuizComponent extends Component {
         <div>
           {this.renderCode}
         </div>
-        <form onSubmit={this.handleSubmit}>
+        <form id="question-form" onSubmit={this.handleSubmit}>
           {this.renderAnswers}
           <input type="submit" value="Submit"/>
         </form>
